06b3c1acb5a2e022195795921da9223a68936250
dm <dmitry@udalov.online>
2025-01-15 19:57:31 +0600

    123

--- features.in/atomic-installer/config.mk
+++ features.in/atomic-installer/config.mk
@@ -1,7 +1,5 @@
 use/atomic-installer:
 	@$(call add_feature)
 	@$(call add,LIVE_PACKAGES,git podman fuse-overlayfs golang btrfs-progs)
-
-use/atomic-installer:
 	@echo "Копирование содержимого rootfs/"
 	@rsync -a features.in/atomic-installer/rootfs/ $(BUILDDIR)/
